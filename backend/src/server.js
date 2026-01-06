3da9ffa8c14f3873d3368bb92cafaa55be2871c2
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-01-06 22:39:43 +0530

    Initial Deployment Code

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,19 @@ import { ENV } from "./lib/env.js"
 
 const app = express()
 
-console.log(ENV.PORT)
-console.log(ENV.DB_URL)
+
 
 app.get("/" , (req , res) => {
     res.status(200).json({msg : "Success from API"})
 })
+
+if(ENV.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname , "../frontend/dist")));
+
+    app.get("/{*any}" , (req,res) => {
+        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"));
+    });
+}
 
 app.listen(ENV.PORT , () => console.log("Server is running on port" , ENV.PORT))
 
