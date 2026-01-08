453a6273866325bd1f637355f7fc551e54feb77b
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-01-08 16:32:56 +0530

    DB Connection method Implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,8 @@
 import express from "express"
 import { ENV } from "./lib/env.js"
+import { connectDB } from "./lib/db.js"
 
 const app = express()
-
-
 
 app.get("/" , (req , res) => {
     res.status(200).json({msg : "Success from API"})
@@ -17,5 +16,13 @@ if(ENV.NODE_ENV == "production") {
     });
 }
 
-app.listen(ENV.PORT , () => console.log("Server is running on port" , ENV.PORT))
+const startServer = async() => {
+    try {
+        connectDB();
+        app.listen(ENV.PORT , () => console.log("Server is running on port" , ENV.PORT))
+    } catch (error) {
+        console.error("Error Starting the Server" , error);
+    }
+};
 
+startServer();
