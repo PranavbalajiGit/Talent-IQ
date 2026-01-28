1c294e2ce3219f77917e91a5180eb8852095c390
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-01-28 23:28:16 +0530

    Auth - Create User in DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from "express"
-import { ENV } from "./lib/env.js"
 import path from "path";
+import cors from "cors";
+
+import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js"
 
 const app = express()
 const __dirname = path.resolve(); 
+
+app.use(express.json());
+app.use(cors({origin : ENV.CLIENT_URL , credentials : true}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
