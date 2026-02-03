cac6bc0f0aa13ff28193516c162f006b233eb182
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-02-03 21:45:34 +0530

    ProtectRoute Implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import path from "path";
 import cors from "cors";
 import {serve} from "inngest/express"
+import {clerkMiddleware} from "@clerk/express"
 
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js"
@@ -12,6 +13,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({origin : ENV.CLIENT_URL , credentials : true}));
+app.use(clerkMiddleware());
 
 app.use("/api/inngest" , serve({ client:inngest , functions }));
 
