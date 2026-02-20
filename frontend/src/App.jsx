155e2c433bf1da201af2bc57397c86d62a56f7ae
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-02-20 23:32:32 +0530

    Features Grid Section , Routing in Home Page and Handling Flickering Effect has been Implemented.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,21 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser }
 import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
 import ProblemsPage from './pages/ProblemsPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
 
-  const {isSignedIn}= useUser();
+  const {isSignedIn , isLoaded}= useUser();
+
+  if(!isLoaded) return null;
 
   return (
     <>
     <Routes>
-      <Route path='/' element = {<HomePage/>}/>
-      <Route path='/problems' element = {isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
+      <Route path='/' element = {!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"}/>}/>
+      <Route path='/dashboard' element = {isSignedIn ? <DashboardPage /> : <Navigate to={"/"}/>}/>
+      <Route path='/problems' element = {isSignedIn ? <ProblemsPage /> : <Navigate to={"/"}/>}/>
     </Routes>
     
     <Toaster />
