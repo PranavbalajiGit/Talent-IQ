eb935e0b79a23e8827f69cb72a747b7b06e2df05
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-02-22 18:29:44 +0530

    Panel Distribution using React Resizable panels implemented.

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -2,6 +2,13 @@ import { useNavigate, useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { PROBLEMS } from "../data/problems";
 
+import Navbar from "../components/Navbar.jsx";
+import ProblemDescription from "../components/ProblemDescription.jsx";
+import OutputPanel from "../components/OutputPanel.jsx";
+import CodeEditorPanel from "../components/CodeEditorPanel.jsx";
+
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+
 function ProblemPage() {
 
     const { id } = useParams();
@@ -32,7 +39,38 @@ function ProblemPage() {
     const checkIfTestsPassed = () => {};
 
   return (
-    <div>ProblemPage</div>
+    <div className="h-screen w-screen bg-base-100 flex flex-col">
+        <Navbar />
+
+        <div className="flex-1">
+            <PanelGroup direction="horizontal">
+
+                {/* LEFT PANEL for PROBLEM DESCRIPTION */}
+                <Panel defaultSize={40} minSize={30}>
+                    <ProblemDescription />
+                </Panel>
+
+                {/* RESIZEABLE PANEL IN BETWEEN FOR PROBLEM DESCRIPTION AND CODE EDITOR */}
+                <PanelResizeHandle className="w-2 bg-base-300 hover:bg-primary transition-colors cursor-col-resize" />
+
+                {/* RIGHT PANEL for CODE EDITOR and OUTPUT */}
+                <Panel defaultSize={60} minSize={30}>
+                    <PanelGroup>
+                        <Panel defaultSize={70} minSize={30}>
+                            <CodeEditorPanel />
+                        </Panel>
+
+                        <PanelResizeHandle className="w-2 bg-base-300 hover:bg-primary transition-colors cursor-row-resize" />
+
+                        <Panel defaultSize={30} minSize={30}>
+                            <OutputPanel />
+                        </Panel>
+                    </PanelGroup>
+                </Panel>
+
+            </PanelGroup>
+        </div>
+    </div>
   )
 }
 
